5e89733c8637d0caa288a759845f297d81a8ff97
Nishan Gunawardena <nishan.nilanka@gmail.com>
2021-07-13 04:22:25 +1000

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,84 @@
+# import the necessary packages
+from imutils.video import FileVideoStream
+from imutils import face_utils
+from imutils.video import FPS
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+# construct the argument parser and parse the arguments
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-p", "--shape-predictor", required=True,
+                #help="path to facial landmark predictor")
+#args = vars(ap.parse_args())
+
+# initialize dlib's face detector (HOG-based) and then create the
+# facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+# initialize the video stream and sleep for a bit, allowing the
+# camera sensor to warm up
+print("[INFO] camera sensor warming up...")
+vs = FileVideoStream("51_1_1.mp4").start()
+# vs = VideoStream(src=1).start()
+# vs = VideoStream(usePiCamera=True).start() # Raspberry Pi
+time.sleep(2.0)
+fps = FPS().start()
+
+
+
+# loop over the frames from the video stream
+while vs.more():
+    # grab the frame from the threaded video stream, resize it to
+    # have a maximum width of 400 pixels, and convert it to
+    # grayscale
+    frame = vs.read()
+    #frame = imutils.resize(frame, width=400)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+    # check to see if a face was detected, and if so, draw the total
+    # number of faces on the frame
+    if len(rects) > 0:
+        text = "{} face(s) found".format(len(rects))
+        cv2.putText(frame, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 0, 255), 2)
+
+    # loop over the face detections
+    for rect in rects:
+        # compute the bounding box of the face and draw it on the
+        # frame
+        (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
+        cv2.rectangle(frame, (bX, bY), (bX + bW, bY + bH),
+                      (0, 255, 0), 1)
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw each of them
+        for (i, (x, y)) in enumerate(shape):
+            #if (i == 37 or i == 38 or i == 40 or i == 41 or i == 43 or i == 44 or i == 47 or i == 46):
+            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+            cv2.putText(frame, str(i + 1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+    # show the frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
